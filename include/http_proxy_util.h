44fc57510363b79651d8e5edba9cdc2f80fbfb1f
Ethan Stoneman <etst9006@colorado.edu>
2024-04-01 20:28:36 -0600

    feature: asynchronous proxy serving

--- include/http_proxy_util.h
+++ include/http_proxy_util.h
@@ -2,11 +2,11 @@
 #define HTTP_PROXY_UTIL_H_
 
 #include <math.h>
+#include <pthread.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/socket.h>
-#include <sys/types.h>
 
+#define CRLF "\r\n"
 #define HTML_400 "./err/400.html"
 #define HTML_404 "./err/404.html"
 
@@ -16,6 +16,7 @@
 #define HTTP_HOSTNAME_MAX 256
 #define HTTP_MAX_ERR_HEADER 1024
 #define HTTP_MAXLINE_CMD (HTTP_METHOD_MAX + HTTP_URI_MAX + HTTP_VERSION_MAX)
+#define HTTP_MAXLINE_HDR HTTP_HEADER_KEY_MAX + HTTP_HEADER_VALUE_MAX
 #define HTTP_METHOD_MAX 8
 #define HTTP_PARAM_KEY_MAX 256
 #define HTTP_PARAM_VALUE_MAX 256
@@ -26,10 +27,10 @@
   (HTTP_HOSTNAME_MAX + HTTP_PORT_MAX_DIGITS + HTTP_REMOTE_MAX)
 #define HTTP_VERSION_MAX 16
 
-#define HTTP_BAD_REQUEST 400
-#define HTTP_NOT_FOUND 404
-#define HTTP_METHOD_NOT_ALLOWED 405
-#define HTTP_VERSION_NOT_SUPPORTED 505
+#define HTTP_BAD_REQUEST_CODE 400
+#define HTTP_NOT_FOUND_CODE 404
+#define HTTP_METHOD_NOT_ALLOWED_CODE 405
+#define HTTP_VERSION_NOT_SUPPORTED_CODE 505
 
 typedef struct {
   char hostname[HTTP_HOSTNAME_MAX];
@@ -38,18 +39,18 @@ typedef struct {
 } HTTPHost;
 
 typedef struct {
-  char param_key[HTTP_PARAM_KEY_MAX];
-  char param_value[HTTP_PARAM_VALUE_MAX];
+  char key[HTTP_PARAM_KEY_MAX];
+  char value[HTTP_PARAM_VALUE_MAX];
 } HTTPQuery;
 
 typedef struct {
-  HTTPHost http_host;
-  HTTPQuery http_query[HTTP_QUERIES_MAX];
+  HTTPHost host;
+  HTTPQuery query[HTTP_QUERIES_MAX];
 } HTTPUri;
 
 typedef struct {
   char method[HTTP_METHOD_MAX];
-  HTTPUri http_uri;
+  HTTPUri uri;
   char version[HTTP_VERSION_MAX];
 } HTTPCommand;
 
@@ -58,26 +59,42 @@ typedef struct {
   char value[HTTP_HEADER_VALUE_MAX];
 } HTTPHeader;
 
+typedef struct {
+  int client_sockfd;
+  int origin_sockfd;
+  char *request;
+  ssize_t len_request;
+  char *response;
+  ssize_t len_response;
+} HTTPProxyState;
+
 char *alloc_buf(size_t);
+void *async_forward_request(void *request);
+void *async_forward_response(void *response);
+char *build_request(HTTPCommand *, HTTPHeader **, size_t, size_t *);
 int chk_alloc_err(void *, const char *, const char *, int);
 ssize_t find_crlf(char *, size_t);
 void handle_connection(int);
-const char *http_status_msg(int);
 ssize_t http_readline(char *, size_t, char *);
+const char *http_status_msg(int);
 ssize_t parse_command(char *, size_t, HTTPCommand *);
-ssize_t parse_headers(char *, ssize_t, HTTPHeader *);
+ssize_t parse_headers(char *, size_t, HTTPHeader *, size_t *);
 ssize_t parse_host(char *, size_t, HTTPHost *);
 ssize_t parse_query(char *, HTTPQuery *);
-ssize_t parse_request(char *, ssize_t, HTTPCommand *, HTTPHeader *);
+ssize_t parse_request(char *, ssize_t, HTTPCommand *, HTTPHeader *, size_t *);
 ssize_t parse_uri(char *, size_t, HTTPUri *);
 char *proxy_recv(int, ssize_t *);
 ssize_t proxy_send(int, char *, size_t);
 char *read_file(const char *, size_t *);
 ssize_t read_until(char *, size_t, char, char *, size_t);
 char *realloc_buf(char *, size_t size);
-int send_err(int, const char *, size_t);
+int send_err(int, size_t);
 size_t skip_scheme(char *);
+int validate_method(char *);
 
 // debug
 void print_command(HTTPCommand);
+void print_header(HTTPHeader);
+void print_headers(HTTPHeader *, size_t);
+void print_request(HTTPHeader *, size_t, HTTPCommand);
 #endif  // HTTP_PROXY_UTIL_H_
