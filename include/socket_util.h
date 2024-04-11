a4846419a05a2b967b59531a230f09827c053638
Ethan Stoneman <etst9006@colorado.edu>
2024-04-11 05:38:56 -0600

    increased recieve chunk size

--- include/socket_util.h
+++ include/socket_util.h
@@ -25,7 +25,7 @@
 #define MAX_RECV_SZ (RECV_CHUNK_SZ * 1000)
 #define RCVTIMEO_SEC 5
 #define RCVTIMEO_USEC (50 * 1000)
-#define RECV_CHUNK_SZ 1024
+#define RECV_CHUNK_SZ 4096
 
 int connection_sockfd(const char *, const char *);
 int listen_sockfd(const char *);
